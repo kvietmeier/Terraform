97e18b37891910e66c657ddef299cac97afa7890
kvietmeier <karlv@storagenet.org>
2023-04-26 15:48:18 -0700

    Got grub modification in cloud-init working

--- azure/VMs/vmtesting/vmtest.main.tf
+++ azure/VMs/vmtesting/vmtest.main.tf
@@ -50,7 +50,7 @@ resource "azurerm_resource_group" "linuxvm_rg" {
 ###--- Setup a cloud-init configuration file - need both parts
 # Refer to the source yaml file
 data "template_file" "system_setup" {
-  template = file("../../secrets/cloud-init")
+  template = file("../../secrets/cloud-init-testgrub")
 }
 
 # Render a multi-part cloud-init config making use of the file above, and other source files if required
