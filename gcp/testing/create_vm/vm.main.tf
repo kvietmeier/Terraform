75304d4d5eac15190fbce877c8e71136936f147a
kvietmeier <karlv@storagenet.org>
2024-10-16 13:47:22 -0700

    VM Creation updates

---
+++ gcp/testing/create_vm/vm.main.tf
@@ -0,0 +1,90 @@
+###===================================================================================###
+#
+#  File:  Template.tf
+#  Created By: Karl Vietmeier
+#
+#  Terraform Module Code
+#  Purpose:  Create a VM
+# 
+#  Files in Module:
+#    main.tf
+#    variables.tf
+#    variables.tfvars
+#
+#  Usage:
+#  terraform apply --auto-approve
+#  terraform destroy --auto-approve
+###===================================================================================###
+
+/* 
+
+Put Usage Documentation here
+
+*/
+
+
+###===================================================================================###
+#     Start creating infrastructure resources
+###===================================================================================###
+
+
+# Network resource (optional: create a new VPC)
+resource "google_compute_network" "vpc_network" {
+  name                    = var.network_name     # Use the network_name variable
+  auto_create_subnetworks = "false"
+}
+
+# Subnetwork
+resource "google_compute_subnetwork" "subnetwork" {
+  region        = var.region          # Use the region variable
+  name          = var.subnet_name     # Use the subnet_name variable
+  ip_cidr_range = var.subnet_cidr     # Use the subnet_cidr variable
+  network       = google_compute_network.vpc_network.id
+}
+
+# Firewall rule to allow only specific IP addresses
+resource "google_compute_firewall" "allow_specific_ip" {
+  name    = "allow-specific-ip"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = var.net_protocol
+    ports    = var.destination_ports 
+  }
+
+  source_ranges = var.whitelist_ips
+}
+
+# Google Cloud VM instance with public IP
+resource "google_compute_instance" "vm_instance" {
+  zone         = var.zone             # Use the zone variable
+  name         = var.vm_name          # Use the vm_name variable
+  machine_type = var.machine_type     # Use the machine_type variable
+
+  boot_disk {
+    initialize_params {
+      image = var.os_image  # Replace with your preferred image
+      size  = var.bootdisk_size
+    }
+  }
+
+  network_interface {
+    network     = google_compute_network.vpc_network.id
+    subnetwork  = google_compute_subnetwork.subnetwork.id
+    access_config {    # This gives the VM a public IP
+      nat_ip = google_compute_address.vm_public_ip.address
+    }
+  }
+
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${local.ssh_key_content}"
+  }
+
+  tags = ["kv-linux", "kv-infra"]
+}
+
+# Reserve a static public IP for the VM
+resource "google_compute_address" "vm_public_ip" {
+  name   = "vm-public-ip"
+  region = var.region                # Use the region variable
+}
---
+++ gcp/testing/create_vm/vm.provider.tf
@@ -0,0 +1,35 @@
+###===================================================================================###
+#
+#  File:  provider.tf
+#  Created By: Karl Vietmeier
+#
+#  Purpose: Configure the GCP Provider TerraForm
+# 
+#  Google defaults set as Env: vars
+#
+###===================================================================================###
+
+
+terraform {
+  required_providers {
+  google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+/*
+provider "google" {
+  project   = "clouddev-itdesk124"
+  #project   = "913067105288"
+  region    = "us-west2"
+  zone      = "us-west2-a"
+}
+*/
+
+provider "google" {
+  project = "clouddev-itdesk124"
+  region  = "us-west2"
+  zone    = "us-west2-a"
+}
