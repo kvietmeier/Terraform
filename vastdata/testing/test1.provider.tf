d4c4579ca9a1ddc2649a5ce62fc853cd80312b8c
kvietmeier <karlv@storagenet.org>
2025-02-28 22:05:27 +0000

    minor tweak

--- vastdata/testing/test1.provider.tf
+++ vastdata/testing/test1.provider.tf
@@ -17,25 +17,25 @@ terraform {
 # Provider Configuration Cluster A - local in GCP
 provider vastdata {
   # Set values in .tfvars
+  #alias                   = "GCPCluster"
+  alias                   = var.vasta_alias
   username                = var.vasta_user
   port                    = var.vasta_port
   password                = var.vasta_passwd
   host                    = var.vasta_host
-  #host                    = "10.100.2.8"
   skip_ssl_verify         = var.vasta_skip_ssl
   version_validation_mode = var.vasta_validation_mode
-  alias                   = "GCPCluster"
 }
 
 # Provider Configuration - Cluster B (remote)
 provider vastdata {
   # Set values in .tfvars
+  #alias                   = "RemoteCluster"
+  alias                   = var.vastb_alias
   username                = var.vastb_user
   port                    = var.vastb_port
   password                = var.vastb_passwd
   host                    = var.vastb_host
-  #host                    = "10.100.1.98"
   skip_ssl_verify         = var.vastb_skip_ssl
   version_validation_mode = var.vastb_validation_mode
-  alias                   = "RemoteCluster"
 }
