592a02824d7a1a8317acf40d83564702db226ad2
Karl Vietmeier <karlv@storagenet.org>
2025-06-24 15:58:09 -0700

    minor changes

--- vastdata/cluster_config/cluster_config.users.tf
+++ vastdata/cluster_config/cluster_config.users.tf
@@ -15,7 +15,8 @@
 ###===================================================================================###
 
 
-/* #Create a tenant with the name tenent01 with client_ip_ranges
+/* 
+#Create a tenant with the name tenent01 with client_ip_ranges
 resource "vastdata_tenant" "tenant1" {
   provider     = vastdata.GCPCluster
   name         = var.tenant1_name
