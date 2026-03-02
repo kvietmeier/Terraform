e5bb2386bf926898aa8ad72e42cdfd682f9c3073
kvietmeier <karlv@storagenet.org>
2026-03-02 08:37:45 -0800

    updated fw

--- gcp/CoreInfra/firewalls/my_rules/fw.variables.tf
+++ gcp/CoreInfra/firewalls/my_rules/fw.variables.tf
@@ -1,15 +1,37 @@
 ###===================================================================================###
+#  File:        fw.variables.tf
+#  Author:      Karl Vietmeier (Refactored by Gemini Code Assist)
 #
 #  File:  fw.variables.tf
 #  Created By: Karl Vietmeier
+#  Purpose:     Variable definitions for custom firewall rules.
 #
 #  Variable definitions with defaults
+#  Structure:
+#    - `ingress_firewall_rules`: A map of rule objects to dynamically create firewall
+#      rules, reducing code duplication.
+#    - `vpc_name`: The target VPC for the firewall rules.
+#    - `ingress_filter`: Source IP ranges for ingress traffic.
+#    - HA VPN specific variables for the BGP peering connection.
 #
 ###===================================================================================###
 
 ###--- Provider Info
 variable "region" {
   description = "Region to deploy resources"
+variable "ingress_firewall_rules" {
+  description = "A map of custom ingress firewall rules to create."
+  type = map(object({
+    name        = string
+    description = string
+    priority    = number
+    allow = list(object({
+      protocol = string
+      ports    = optional(list(string))
+    }))
+    target_tags = optional(list(string))
+  }))
+  default = {}
 }
 
 variable "zone" {
@@ -24,15 +46,21 @@ variable "project_id" {
 ###--- VPC Setup
 variable "vpc_name" {
   description = "Name of VPC"
+  description = "The name of the VPC network where firewall rules will be applied."
   type        = string
   default     = "default"
 }
 
+variable "ingress_filter" {
+  description = "A list of source CIDR ranges to allow for ingress rules."
+  type        = list(string)
+}
 
 ###--- Firewall
 
 variable "ingress_rule" {
   description = "Ingress"
+  description = "The direction for the firewall rules (e.g., INGRESS)."
   type        = string
   default     = "INGRESS"
 }
@@ -151,10 +179,12 @@ variable vast_udp {
 # --- HA VPN Control Plane Variables ---
 variable "azure_public_ip_01" {
   description = "The public IP of the first Azure VPN Gateway interface."
+  description = "Public IP of the first Azure VPN gateway for HA VPN."
   type        = string
 }
 
 variable "azure_public_ip_02" {
   description = "The public IP of the second Azure VPN Gateway interface."
+  description = "Public IP of the second Azure VPN gateway for HA VPN."
   type        = string
 }
